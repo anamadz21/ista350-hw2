8fbfa2d12518cad9f847d68a7778eec20b1f7f5d
ana <anab@email.arizona.edu>
2014-02-09 16:18:32 -0700

    test

---
+++ myturtle.py
@@ -0,0 +1,38 @@
+class Myturtle(Turtle)
+    def __init__(self)
+    
+    """Constructs a new MyTurtle object.
+Initializes the drawing environment (by calling Turtle’s constructor).
+Initializes name, an instance variable, from user input.
+Initializes a dictionary with vital info about the MyTurtle."""
+
+    def _get_dict(self)
+    """Return a three-pair dictionary holding MyTurtle's vital info based on user input.
+One key must be 'sociality', which can only take on one of the following values:
+'friendly', 'aloof', or 'aggressive' – keep prompting the user until they
+enter a valid value.
+Another key is 'age' in years (an integer); the last is favorite 'food' (a string).
+Remember that input() always returns a string, so you will have to do a type
+conversion."""
+
+    def report(self)
+
+        """Generates a report on MyTurtle per the following template:
+MyTurtle report on <name>:
+--------------------------
+Age: <age>
+Favorite food: <favorite food>
+Sociality: <sociality>
+This report is generated from the turtle's vital info"""
+
+    def draw_name(self)
+    
+
+
+    draw_R(turt, x=0, y=0)
+    
+    draw_i(turt, x=0, y=0)
+    
+    draw_c(turt, x=0, y=0)
+
+    draw_h(turt, x=0, y=0)
