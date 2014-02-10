1ad77cf6084cc35c1dd6837f54af8595d2ddc802
ana <anab@email.arizona.edu>
2014-02-10 12:14:10 -0700

    test

--- myturtle.py
+++ myturtle.py
@@ -1,38 +1,16 @@
-﻿class Myturtle(Turtle)
-    def __init__(self)
-    
-    """Constructs a new MyTurtle object.
-Initializes the drawing environment (by calling Turtle’s constructor).
-Initializes name, an instance variable, from user input.
-Initializes a dictionary with vital info about the MyTurtle."""
+﻿import turtle
+class MyTurtle(turtle.Turtle):
 
-    def _get_dict(self)
-    """Return a three-pair dictionary holding MyTurtle's vital info based on user input.
-One key must be 'sociality', which can only take on one of the following values:
-'friendly', 'aloof', or 'aggressive' – keep prompting the user until they
-enter a valid value.
-Another key is 'age' in years (an integer); the last is favorite 'food' (a string).
-Remember that input() always returns a string, so you will have to do a type
-conversion."""
+    def __init__(self, name, sociality, age, food):
 
-    def report(self)
+        key = ['name', 'sociality', 'age', 'food']
+        if age ==0:
+            raise ValueError("0 and any other characters are invalid")
+        value = [name, sociality, age, food]
+	
+        self.my_turtle_dict = dict(zip(key, value))
 
-        """Generates a report on MyTurtle per the following template:
-MyTurtle report on <name>:
---------------------------
-Age: <age>
-Favorite food: <favorite food>
-Sociality: <sociality>
-This report is generated from the turtle's vital info"""
+    def _get_dict(self):
 
-    def draw_name(self)
-    
-
-
-    draw_R(turt, x=0, y=0)
-    
-    draw_i(turt, x=0, y=0)
-    
-    draw_c(turt, x=0, y=0)
-
-    draw_h(turt, x=0, y=0)
+    def report(self):
+        
